bdda24f37a27a6e6e2ad3882bf67c27d77382ce8
Vita <vita.ievleva@gmail.com>
2015-08-31 22:11:03 +0300

    Optimized tests for List interface

--- src/test/java/collectionsImpl/ListTests.java
+++ src/test/java/collectionsImpl/ListTests.java
@@ -1,29 +1,24 @@
 package collectionsImpl;
 
 import interfaces.CustomList;
-import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Iterator;
 import java.util.stream.Stream;
 
+import static org.junit.Assert.*;
+
 
 public abstract class ListTests {
 
-    private CustomList<String> list = getList();
+    private CustomList<String> list;
 
     abstract CustomList getList();
 
     @Before
     public void setUp() {
         list = getList();
-    }
-
-    @After
-    public void tearDown() {
-        list = null;
     }
 
 
@@ -35,11 +30,11 @@ public abstract class ListTests {
         list.add("FourthElement");
         list.add(null);
 
-        Assert.assertEquals("FirstElement", list.get(0));
-        Assert.assertEquals("SecondElement", list.get(1));
-        Assert.assertEquals("FourthElement", list.get(3));
-        Assert.assertNull(list.get(4));
-        Assert.assertEquals(list.size(), 5);
+        assertEquals("FirstElement", list.get(0));
+        assertEquals("SecondElement", list.get(1));
+        assertEquals("FourthElement", list.get(3));
+        assertNull(list.get(4));
+        assertEquals(list.size(), 5);
 
     }
 
@@ -52,12 +47,12 @@ public abstract class ListTests {
         list.add(4, null);
         list.add(5, null);
 
-        Assert.assertEquals("FirstElement", list.get(0));
-        Assert.assertEquals("SecondElement", list.get(1));
-        Assert.assertEquals("FourthElement", list.get(3));
-        Assert.assertNull(list.get(4));
-        Assert.assertNull(list.get(5));
-        Assert.assertEquals(list.size(), 6);
+        assertEquals("FirstElement", list.get(0));
+        assertEquals("SecondElement", list.get(1));
+        assertEquals("FourthElement", list.get(3));
+        assertNull(list.get(4));
+        assertNull(list.get(5));
+        assertEquals(list.size(), 6);
     }
 
     @Test
@@ -66,7 +61,7 @@ public abstract class ListTests {
         list.add(1, "SecondElement");
         list.clear();
 
-        Assert.assertEquals(0, list.size());
+        assertEquals(0, list.size());
 
     }
 
@@ -75,12 +70,12 @@ public abstract class ListTests {
         list.add(0, "FirstElement");
         list.add(1, "SecondElement");
 
-        Assert.assertTrue(list.contains("FirstElement"));
-        Assert.assertFalse(list.contains("SomeElement"));
-        Assert.assertFalse(list.contains(null));
+        assertTrue(list.contains("FirstElement"));
+        assertFalse(list.contains("SomeElement"));
+        assertFalse(list.contains(null));
 
         list.add(1, null);
-        Assert.assertTrue(list.contains(null));
+        assertTrue(list.contains(null));
     }
 
     @Test
@@ -91,9 +86,9 @@ public abstract class ListTests {
         list.add(3, "ThirdElement");
         list.add(4, null);
 
-        Assert.assertEquals(list.indexOf("ThirdElement"), 2);
-        Assert.assertEquals(list.indexOf(null), 4);
-        Assert.assertEquals(list.indexOf("SomeElement"), -1);
+        assertEquals(list.indexOf("ThirdElement"), 2);
+        assertEquals(list.indexOf(null), 4);
+        assertEquals(list.indexOf("SomeElement"), -1);
     }
 
     @Test
@@ -103,9 +98,9 @@ public abstract class ListTests {
         list.add(2, null);
         list.add(3, null);
 
-        Assert.assertEquals(list.lastIndexOf("FirstElement"), 1);
-        Assert.assertEquals(list.lastIndexOf(null), 3);
-        Assert.assertEquals(list.lastIndexOf(""), -1);
+        assertEquals(list.lastIndexOf("FirstElement"), 1);
+        assertEquals(list.lastIndexOf(null), 3);
+        assertEquals(list.lastIndexOf(""), -1);
 
     }
 
@@ -118,16 +113,16 @@ public abstract class ListTests {
         list.add(4, "4");
         list.add(5, "5");
 
-        Assert.assertEquals("5", list.remove(5));
-        Assert.assertEquals(5, list.size());
-        Assert.assertEquals("4", list.remove(4));
-        Assert.assertEquals(4, list.size());
-        Assert.assertEquals(null, list.remove(2));
-        Assert.assertEquals(3, list.size());
-        Assert.assertEquals("FirstElement", list.remove(0));
+        assertEquals("5", list.remove(5));
+        assertEquals(5, list.size());
+        assertEquals("4", list.remove(4));
+        assertEquals(4, list.size());
+        assertEquals(null, list.remove(2));
+        assertEquals(3, list.size());
+        assertEquals("FirstElement", list.remove(0));
 
         list.remove(1);
-        Assert.assertEquals(1, list.size());
+        assertEquals(1, list.size());
 
     }
 
@@ -140,9 +135,9 @@ public abstract class ListTests {
         list.add(4, "4");
         list.add(5, "5");
 
-        Stream.of("1", "2", "3", "4", "5").forEach(item -> Assert.assertEquals(true, list.remove(item)));
+        Stream.of("1", "2", "3", "4", "5").forEach(item -> assertEquals(true, list.remove(item)));
 
-        Assert.assertEquals(1, list.size());
+        assertEquals(1, list.size());
 
     }
 
@@ -155,37 +150,34 @@ public abstract class ListTests {
         list.add(4, "4");
         list.add(5, "5");
 
-        Assert.assertEquals("0", list.set(0, "Zero"));
-        Assert.assertEquals(6, list.size());
-        Assert.assertEquals("5", list.set(5, "Five"));
-        Assert.assertEquals(6, list.size());
+        assertEquals("0", list.set(0, "Zero"));
+        assertEquals(6, list.size());
+        assertEquals("5", list.set(5, "Five"));
+        assertEquals(6, list.size());
     }
 
     @Test
     public void testSize() throws Exception {
-        Assert.assertEquals(0, list.size());
+        assertEquals(0, list.size());
 
         list.add(0, "0");
-        Assert.assertEquals(1, list.size());
+        assertEquals(1, list.size());
     }
 
     @Test
     public void testToArray() throws Exception {
         list.add(0, "FirstElement");
-        list.add(1, "SecondElement");
-        list.add(2, "ThirdElement");
-        list.add(3, "FourthElement");
-        list.add(4, null);
+        list.add(1, null);
         Object[] array = list.toArray();
 
-        Assert.assertEquals(array[0], "FirstElement");
-        Assert.assertNull(array[4]);
+        assertEquals(array[0], "FirstElement");
+        assertNull(array[1]);
     }
 
     @Test
     public void testGet() throws Exception {
         list.add(0, "FirstElement");
-        Assert.assertEquals(list.get(0), "FirstElement");
+        assertEquals(list.get(0), "FirstElement");
     }
 
     @Test
@@ -196,19 +188,21 @@ public abstract class ListTests {
         list.add(2, "ThirdElement");
         list.add(3, "FourthElement");
 
+        String[] expected = {"FirstElement", "SecondElement", "ThirdElement", "FourthElement"};
+
         Iterator<String> itr = list.iterator();
 
         while (itr.hasNext()) {
-            Assert.assertEquals(list.get(i++), itr.next());
+            assertEquals(expected[i++], itr.next());
         }
     }
 
     @Test
     public void testIsEmpty() throws Exception {
-        Assert.assertTrue(list.isEmpty());
+        assertTrue(list.isEmpty());
 
         list.add(0, "FirstElement");
-        Assert.assertFalse(list.isEmpty());
+        assertFalse(list.isEmpty());
     }
 
 }
